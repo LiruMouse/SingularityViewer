880733d2cea355bef91986be5694828c90851932
lee <lee@yun.yagibdah.de>
2015-07-24 18:11:33 +0200

    show how long it takes to create a frame instead of FPS rate
         This one is for debugging.

--- indra/newview/llviewerwindow.cpp
+++ indra/newview/llviewerwindow.cpp
@@ -313,7 +313,8 @@ public:
 		static const LLCachedControl<bool> slb_show_fps("SLBShowFPS");
 		if (slb_show_fps)
 		{
-			addText(xpos+280, ypos+5, llformat("FPS %3.1f", LLViewerStats::getInstance()->mFPSStat.getMeanPerSec()));
+			// addText(xpos+280, ypos+5, llformat("FPS %3.1f", LLViewerStats::getInstance()->mFPSStat.getMeanPerSec()));
+			addText(xpos+200, ypos+5, llformat("FIS %10.6f", gFrameIntervalSeconds));
 			ypos += y_inc;
 		}
 
